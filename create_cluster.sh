57d53f7af85365f6fbf78f134f20a1f8a664b4b2
unknown <xcgoner1108@gmail.com>
2020-04-13 14:41:02 -0500

    ec2 scripts

--- create_cluster.sh
+++ create_cluster.sh
@@ -3,6 +3,7 @@ HOSTFILE=$1
 while read -u10 HOST
 do
 echo $HOST
+ssh $HOST "mkdir -p $PWD" 2>&1 > ec2.log
 scp update_cluster.sh $HOST:$PWD
 ssh $HOST "bash $PWD/update_cluster.sh" 2>&1 > ec2.log
 done 10< "$HOSTFILE"
